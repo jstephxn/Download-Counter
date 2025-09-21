426ad779b27d0a0ca382a957036c9f951bd5eb12
jstephxn <151784995+jstephxn@users.noreply.github.com>
2025-09-21 11:24:54 +0100

    initial commit

---
+++ Download-Counter.py
@@ -0,0 +1,98 @@
+import os 
+import pandas as pd
+import reportlab as rprt
+from pathlib import Path
+
+
+class FileEplorer(object):
+    
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def read_file(self):
+
+        # Create an empty list to store the data from the csv file 
+        file_contents = []
+
+        # Save the filename with the .csv file extension 
+        file_name = self.file_name + ".csv"
+
+        # Create a path to find the given file
+        csv_path = Path.home() / "Downloads" / file_name
+
+        # ADD CODE TO CHECK IF FILE EXISTS
+        # - - - - - - - - - - - - - - - - - - - - - - - - -
+        # IF FILE EXISTS THEN OPEN AND READ FROM FILE
+        raw_data = pd.read_csv(csv_path)
+
+        # Read from the collumn containing the download message
+        for row in raw_data['Message']:
+            
+            file_contents.append(row)
+        
+        return file_contents
+    
+        # ELSE RETURN ERROR 
+        # - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class StringSplicer(object):
+    
+    def __init__(self):
+        self = self
+        
+    def get_subject(self):
+        pass
+
+    def get_resource_name(string):
+        pass
+
+    def get_resource_link(self):
+        pass
+
+class Calculate(object): 
+    
+    def __init__(self, contents_list):
+        self.contents_list = contents_list
+    
+    def total():
+
+        # Create empty dictionary to store the totals with the resource name/ subject as the key and the count as the values 
+        download_count = {}
+        subject_count = {}
+
+        for item in Calculate.contents_list:
+            
+            resource_name = StringSplicer.get_resource_name(item) 
+            resource_link = StringSplicer.get_resource_link(item)
+
+            if resource_name not in download_count:
+
+                # Count how many times the resource name is fount in the list 
+                count = sum(1 for content in Calculate.contents_list if resource_name and resource_link in content)
+
+                # Store Count in Dictionary 
+                download_count[resource_name] = count
+
+            else:
+
+                continue
+        
+        for item in Calculate.contents_list:
+            
+            # Get the name of the subject of the resource
+            subject_name = str(StringSplicer.get_subject(item))
+
+            if subject_name not in subject_count:
+                
+                # Count how many times the subject name is found in the list
+                count = sum(1 for subject in Calculate.contents_list if subject_name in subject)
+
+                # Store count in Dictionary 
+                subject_count[subject_name] = count
+
+
+        return download_count, subject_count  
+            
+
+            
+
